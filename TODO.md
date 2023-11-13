4d6df707f3fadabc7bd78d20c9519ba7ead34446
sevval <sevylvalmz@gmail.com>
2023-11-13 20:43:24 +0300

    added addition method for doubles

---
+++ TODO.md
@@ -0,0 +1,2 @@
+*[ ] Finalize Git repository setup
+*[ ] Provide an addition method in calculator.java
--- calculator.java
+++ calculator.java
@@ -1,2 +1,5 @@
 public clas calculator{}
 public static void main(Stirng[] args) {}
+ public double addition(double a, douvle b) {/
+retun b+a;/
+}
