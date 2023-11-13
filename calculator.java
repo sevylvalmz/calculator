ec089958212668b4aa4feefa550649dc2e0b336f
sevval <sevylvalmz@gmail.com>
2023-11-13 21:08:58 +0300

    added subtraction method

--- calculator.java
+++ calculator.java
@@ -1,11 +1,11 @@
-public clas calculator{
-public int addition(int a, int b ){ 
-return a+b;
-
-}
-
- public double addition(double a, douvle b) {/
-retun b+a;/
-}
-}
-jncdlblefnenfefnqwn
+public class Calculator {
+2 // Other methods are hidden for brevity
+3
+4 public int subtraction(int a, int b) {
+5 return a - b;
+6 }
+7
+8 public double subtraction(double a, double b) {
+9 return a - b;
+10 }
+11 }
