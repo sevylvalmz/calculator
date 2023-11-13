21f10680408798b1c2c8214114804dd62d7950a8
sevval <sevylvalmz@gmail.com>
2023-11-13 21:11:46 +0300

    added application to call the calculator

---
+++ Application.java
@@ -0,0 +1,9 @@
+1public class Application {
+2  public static void main(String[] args) {
+3    Calculator calculator = new Calculator();
+4    System.out.println(calculator.addition(1, 2)); \
+5 // Should be 3
+6    System.out.println(calculator.addition(1.5, 3.2\
+7 )); // Should be 4.7
+8     }
+9 }
